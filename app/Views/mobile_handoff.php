86ac85d61245c2256d1d9ca928ff5c99172279fc
Doni Nurramdan <33200117+nurramdandoni@users.noreply.github.com>
2026-03-05 15:20:55 +0700

    Enhance mobile handoff script for app redirection if not have application intalled
    Updated script to handle mobile user agents and added a fallback mechanism for app redirection.

--- app/Views/mobile_handoff.php
+++ app/Views/mobile_handoff.php
@@ -1,7 +1,26 @@
-<script>
+<!-- <script>
 if (navigator.userAgent.includes("PayliteApp")) {
     window.location = "paylite://login?tokenApps=<?= $_COOKIE['tokenApps']; ?>";
 } else {
     window.location = "/loginC";
+}
+</script> -->
+
+<script>
+if (navigator.userAgent.includes("Mobile")) {
+
+    const token = "<?= urlencode($_COOKIE['tokenApps']); ?>";
+    const url = "paylite://callback?tokenApps=" + token;
+
+    // coba buka app
+    window.location.href = url;
+
+    // kalau app tidak ada -> fallback cepat
+    setTimeout(function () {
+        window.location.href = "/loginC";
+    }, 5000);
+
+} else {
+    window.location.href = "/loginC";
 }
 </script>
